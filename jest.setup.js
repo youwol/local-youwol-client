ac18719234715ee56d388111972862ed5543055d
youwol-guillaume <greinisch@youwol.com>
2023-02-21 10:19:01 +0100

    ✅ 🔇 remove log to debug CI failure

--- jest.setup.js
+++ jest.setup.js
@@ -1,12 +1,6 @@
 const jasmine = self.jasmine
 
 jasmine.getEnv().addReporter({
-    specStarted: (result) => {
-        console.log('CustomReporter.specStarted', result)
-        jasmine.currentTest = result
-    },
-    specDone: (result) => {
-        console.log('CustomReporter.specDone', result)
-        jasmine.currentTest = result
-    },
+    specStarted: (result) => (jasmine.currentTest = result),
+    specDone: (result) => (jasmine.currentTest = result),
 })
