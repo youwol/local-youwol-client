d8da500534de5aba0dae59af787c37524ab1d36b
Julien Décharne <jdecharne@youwol.com>
2023-05-16 17:43:34 +0200

    💚 Temporary use py-youwol:feature/refactor

--- src/tests/projects.test.ts
+++ src/tests/projects.test.ts
@@ -64,7 +64,7 @@ beforeAll(async (done) => {
                     name: 'clone-project',
                     body: {
                         url: 'https://github.com/youwol/todo-app-js.git',
-                        branch: 'master',
+                        branch: 'feature/refactor',
                         name: projectName,
                     },
                 }),
