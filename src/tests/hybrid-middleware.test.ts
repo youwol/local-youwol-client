8c7b3cd1b663ac5c480b68d9daee9d8475b022c0
youwol-guillaume <greinisch@youwol.com>
2023-12-05 11:01:04 +0100

    ✅ `hybrid-middleware.test` => add test w/ coupled loading graph
    In this test we try to resolve the loading graph of `rx-tree-views`.
    It depends on `rxjs#^7.5.6`, `rx-vdom#^1.0.1`.
    We put in local a fake version of `rxjs` : `7.999.0` (to ensure that it is resolved as the last of API key v7).
    We expect a resolved loading graph, with rxjs at version `7.999.0`.

--- src/tests/hybrid-middleware.test.ts
+++ src/tests/hybrid-middleware.test.ts
@@ -11,9 +11,12 @@ import {
 } from './shell'
 import { setup$ } from './local-youwol-test-setup'
 import { PyYouwolClient } from '../lib'
-import { delay, take, tap } from 'rxjs/operators'
-import { BehaviorSubject, of } from 'rxjs'
+import { delay, map, mergeMap, take, tap } from 'rxjs/operators'
+import { BehaviorSubject, from, of } from 'rxjs'
 import { DownloadEvent } from '../lib/routers/system'
+import { CdnBackend } from '@youwol/http-clients'
+import { uploadPackagesAndCheck$ } from './utils'
+import path from 'path'
 
 jest.setTimeout(20 * 1000)
 
@@ -84,6 +87,114 @@ test('can retrieve asset info when remote only', (done) => {
         )
         .subscribe(() => {
             expect(downloadEvents$.value).toBeFalsy()
+            done()
+        })
+})
+
+test('coupled loading graph', (done) => {
+    /**
+     * In this test we try to resolve the loading graph of `rx-tree-views`.
+     * It depends on `rxjs#^7.5.6`, `rx-vdom#^1.0.1`.
+     * We put in local a fake version of `rxjs` : `7.999.0` (to ensure that it is resolved as the last of API key v7).
+     * We expect a resolved loading graph, with rxjs at version `7.999.0`.
+     */
+    class Context {
+        // the creation of the test data is gathered in the youwol-config.py
+        public readonly assetId = remoteStoryAssetId
+    }
+    const cdnClient = new CdnBackend.Client()
+    const basePath = './data/test-packages'
+    const target = {
+        graphType: 'sequential-v2',
+        lock: [
+            {
+                name: '@youwol/rx-tree-views',
+                version: '0.3.1',
+                id: 'QHlvdXdvbC9yeC10cmVlLXZpZXdz',
+                namespace: 'youwol',
+                type: 'library',
+                fingerprint: 'ccffc5e9343e4193966a939c0c715311',
+                exportedSymbol: '@youwol/rx-tree-views',
+                aliases: [],
+                apiKey: '03',
+            },
+            {
+                name: 'rxjs',
+                version: '7.999.0',
+                id: 'cnhqcw==',
+                namespace: 'rxjs',
+                type: 'library',
+                fingerprint: 'a413cd90a586944a2f98afb399ca7160',
+                exportedSymbol: 'rxjs',
+                aliases: [],
+                apiKey: '7',
+            },
+            {
+                name: '@youwol/rx-vdom',
+                version: '1.0.1',
+                id: 'QHlvdXdvbC9yeC12ZG9t',
+                namespace: 'youwol',
+                type: 'library',
+                fingerprint: '9bb534b31cc287963fbb39a03c3016d5',
+                exportedSymbol: '@youwol/rx-vdom',
+                aliases: [],
+                apiKey: '1',
+            },
+        ],
+        definition: [
+            [
+                ['cnhqcw==', 'cnhqcw==/7.999.0/dist/rxjs.js'],
+                [
+                    'QHlvdXdvbC9yeC12ZG9t',
+                    'QHlvdXdvbC9yeC12ZG9t/1.0.1/dist/@youwol/rx-vdom.js',
+                ],
+            ],
+            [
+                [
+                    'QHlvdXdvbC9yeC10cmVlLXZpZXdz',
+                    'QHlvdXdvbC9yeC10cmVlLXZpZXdz/0.3.1/dist/@youwol/rx-tree-views.js',
+                ],
+            ],
+        ],
+    }
+    shell$<Context>(new Context())
+        .pipe(
+            mergeMap((shell) => {
+                return uploadPackagesAndCheck$({
+                    packageName: 'rxjs',
+                    paths: {
+                        '7.999.0': path.resolve(
+                            __dirname,
+                            `${basePath}/rxjs#7.999.0.zip`,
+                        ),
+                    },
+                    folderId: shell.homeFolderId,
+                    cdnClient,
+                    check: 'strict',
+                }).pipe(map((_resp) => shell))
+            }),
+            mergeMap(() => {
+                const body = {
+                    libraries: [
+                        { name: '@youwol/rx-tree-views', version: '0.3.1' },
+                    ],
+                }
+                return from(
+                    fetch(
+                        'http://localhost:2001/api/assets-gateway/cdn-backend/queries/loading-graph',
+                        {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json',
+                            },
+                            body: JSON.stringify(body),
+                        },
+                    ).then((resp) => resp.json()),
+                )
+            }),
+        )
+        .subscribe((resp) => {
+            expect(resp).toEqual(target)
             done()
         })
 })
