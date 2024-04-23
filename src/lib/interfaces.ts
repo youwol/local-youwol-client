4ee4df80b8e1621524fe94e29715710583ef0013
youwol-guillaume <greinisch@youwol.com>
2024-04-23 08:51:22 +0200

    🐛 [system] add/fix attributes in `Log` related responses.

--- src/lib/interfaces.ts
+++ src/lib/interfaces.ts
@@ -7,12 +7,13 @@ export type Label = LocalYouwol.Label
 
 export interface ContextMessage<T = unknown> {
     contextId: string
-    level: string
+    level: 'INFO' | 'WARNING' | 'ERROR'
     text: string
     labels: Label[]
     parentContextId: string | undefined
     data: T
     attributes: { [key: string]: string }
+    timestamp: number
 }
 
 export type GetFileContentResponse = string
